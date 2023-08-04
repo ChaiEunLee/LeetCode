1306ab3724ce71e56e7786120ab459666ee741db
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-08-04 13:54:55 +0900

    Time: 287 ms (93.91%), Space: 19.2 MB (48.39%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,20 +4,21 @@ class Solution:
         if not grid:
             return 0
         
+        visited = [[False for i in range(len(grid[0]))] for j in range(len(grid))]
         island = 0
         
-        def dfs(grid, i, j):
-            if 0<=i<len(grid) and 0<=j<len(grid[0]) and grid[i][j] == "1":
-                grid[i][j] = 0
-                dfs(grid, i-1, j)
-                dfs(grid, i+1, j)
-                dfs(grid, i, j-1)
-                dfs(grid, i, j+1)
+        def dfs(i, j):
+            if 0<=i<len(grid) and 0<=j<len(grid[0]) and grid[i][j] == "1" and visited[i][j] == False:
+                visited[i][j] = True
+                dfs(i-1, j)
+                dfs(i+1, j)
+                dfs(i, j-1)
+                dfs(i, j+1)
         
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == "1":
-                    dfs(grid, i, j)
+                if grid[i][j] == "1" and visited[i][j] == False:
+                    dfs(i, j)
                     island += 1
 
         return island

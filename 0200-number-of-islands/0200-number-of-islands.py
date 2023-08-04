5f7f9d711ff2780d1661c978f499d01731eee80b
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-08-04 13:53:17 +0900

    Time: 267 ms (98.60%), Space: 18.9 MB (78.79%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,23 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-        
-        visited = [[False for i in range(len(grid[0]))] for j in range(len(grid))]
-        island = 0
-        
-        def dfs(grid, i, j):
-            if 0<=i<len(grid) and 0<=j<len(grid[0]) and grid[i][j] == "1" and visited[i][j] == False:
-                visited[i][j] = True #꼭 필요함!
-                dfs(grid, i-1, j)
-                dfs(grid, i+1, j)
-                dfs(grid, i, j-1)
-                dfs(grid, i, j+1)
-        
+        def dfs(i, j):
+            #더 이상 땅이 아닌 경우 stop
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != "1":
+                return
+            
+            grid[i][j] = 0
+            dfs(i+1,j)
+            dfs(i-1,j)
+            dfs(i,j-1)
+            dfs(i,j+1)
+
+
+        count = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == "1" and visited[i][j] == False:
-                    dfs(grid, i, j)
-                    island += 1
+                if grid[i][j] == "1":
+                    dfs(i,j)
+                    count += 1
 
-        return island
+        return count

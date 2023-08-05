b19f24c72311d1be238376eae013f49e32fbe71b
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-08-05 10:56:44 +0900

    Time: 78 ms (89.72%), Space: 16.9 MB (9.43%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -3,17 +3,19 @@ class Solution:
         origincolor = image[sr][sc]
         
         def dfs(i,j):
-            if i<0 or i>=len(image) or j<0 or j>=len(image[0]):
+#            if i<0 or i>=len(image) or j<0 or j>=len(image[0]):
+#                return
+#            if image[i][j] == color or image[i][j] != origincolor:
+#                return
+            if 0<=i<len(image) and 0<=j<len(image[0]) and image[i][j] == origincolor and image[i][j] != color:
+                image[i][j] = color
+                dfs(i-1,j)
+                dfs(i+1,j)
+                dfs(i,j-1)
+                dfs(i,j+1)
+            else:
                 return
-            if image[i][j] == color or image[i][j] != origincolor:
-                return
-#            if 0<=i<len(image) and 0<=j<len(image[0]) and image[i][j] == origincolor:
-            image[i][j] = color
-            dfs(i-1,j)
-            dfs(i+1,j)
-            dfs(i,j-1)
-            dfs(i,j+1)
-
+            
         dfs(sr,sc)
                 
         return image

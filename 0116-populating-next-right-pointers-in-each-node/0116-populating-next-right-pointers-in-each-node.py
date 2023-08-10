27d0c800eafbfe33403e2c9eea04ba94ea3274b4
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-08-10 11:01:30 +0900

    Time: 64 ms (84.90%), Space: 18 MB (90.15%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -7,22 +7,29 @@ class Node:
         self.right = right
         self.next = next
 """
-from collections import deque
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        # there always the case where root node is None
         if not root:
-            return None
-        q = deque([root])
-        while q:
-            rightNode = None
-            for _ in range(len(q)):
-                cur = q.popleft()
-                cur.next, rightNode = rightNode, cur
-                if cur.right:
-                    q.extend([cur.right, cur.left])
+            return root
+            
+        queue = collections.deque([root])
+        visited = []
+        while queue:
+            prev = None
+            # number of nodes in the level - when we want to go over all the elements
+            size = len(queue)
+            while size>0:
+                node = queue.popleft()
+                # setting the next pointer
+                # my next pointer is previous, set previous as me for the next node
+                node.next = prev
+                prev = node
+                # For its child
+                if node.right:
+                    queue.append(node.right)
+                if node.left:
+                    queue.append(node.left)
+                size -= 1
         return root
-
-        
-        
-        

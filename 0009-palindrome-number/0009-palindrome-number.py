3a0913f67a6db7992febf6bab2832d0e4c9f17b3
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-08-20 10:36:53 +0900

    Time: 76 ms (41.07%), Space: 16.4 MB (37.96%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,11 +1,14 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0 or (x > 0 and x%10 == 0):   # if x is negative, return False. if x is positive and last digit is 0, that also cannot form a palindrome, return False.
+        if (x<0 or (x>0 and x%10 == 0)):
             return False
+        
+        check = 0
+        while(x>check):
+            check = check*10 + x%10
+            x = x//10
 
-        result = 0
-        while x > result:
-            result = result * 10 + x % 10
-            x = x // 10
-
-        return True if (x == result or x == result // 10) else False
+        if (x==check or x==check//10):
+            return True
+        else:
+            return False

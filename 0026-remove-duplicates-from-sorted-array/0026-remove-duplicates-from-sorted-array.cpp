31a4bc12007ab4636f9600f863d0bc1f2b77818b
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-08-24 16:36:37 +0900

    Time: 11 ms (60.47%), Space: 18.3 MB (58.18%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int uniqcnt = 0;
-        for (int i=0; i<nums.size(); i++){
-            if (ifDup(nums[i], nums, i)){
-                nums[uniqcnt] = nums[i];
-                uniqcnt++;
+        int count = 0;
+        for (int i=1; i<nums.size(); i++){
+            if(nums[i] == nums[i-1]){
+                count++;
+            } else {
+                nums[i-count] = nums[i];
             }
-        }
-        return uniqcnt;        
-    }
-
-    bool ifDup(int x, vector<int>& nums, int index){
-        for(int i=0; i<index; i++){
-            if(nums[i] == x){
-                return false;
-            } 
-        }
-        return true;
+        }      
+        return nums.size()-count;
     }
 };

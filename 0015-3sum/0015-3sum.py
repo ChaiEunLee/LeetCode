a2954253f14cfe28b8494743e036690354c0c2a8
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-08-26 10:16:16 +0900

    Time: 1043 ms (77.02%), Space: 20.1 MB (98.93%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,43 +1,24 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = set() # 중복제거하려고
-
-        # 1. Split nums into three lists: negative numbers, positive numbers, and zeros
-        n,p,z = [],[],[]
-        for num in nums:
-            if num > 0:
-                p.append(num)
-            elif num < 0:
-                n.append(num)
-            else:
-                z.append(num)
-
-        # 2. Create a separate set for negatives and positives for O(1) look-up times
-        N,P = set(n), set(p)
-
-        # 3. If there is at least 1 zero in the list, add all cases where -num exists in N and num exists in P
-        # i.e. (-3,0,3) = 0
-        if z:
-            for num in P:
-                if -1*num in N:
-                    res.add((-1*num, 0, num))
-
-        # 3. If there are at least 3 zeros in the list then also includ (0,0,0) = 0
-        if len(z) >= 3:
-            res.add((0,0,0))
-
-        # 4. For all pairs of negative numbers (-3,-1), check to see if their complement (4) exists in the positive number set
-        for i in range(len(n)):
-            for j in range(i+1, len(n)):
-                target = -1*(n[i]+n[j])
-                if target in P:
-                    res.add(tuple(sorted([n[i],n[j],target])))
-
-        # 5. For all pairs of positive numbers (1,1), check to see if their complement (-2) exists in the negative number set
-        for i in range(len(p)):
-            for j in range(i+1, len(p)):
-                target = -1*(p[i]+p[j])
-                if target in N:
-                    res.add(tuple(sorted([p[i], p[j], target])))
-        
-        return res 
+        res = []
+        nums.sort() # sort
+        for i in range(len(nums)-2): # need 3 numbers, 그래서 len(nums)-2까지 range
+            l,r = i+1, len(nums)-1 # i번째, i-1번째 숫자가 다르면 l,r two pointer 지정. l은 i+1 번째, r은 맨 오른쪽 끝으로 지정.
+            if i==0 or nums[i] != nums[i-1]: # i번째, i-1번째 숫자가 같으면 아래 코드 skip    
+                while l<r: # l,r 만나기 전까지
+                    s = nums[i] + nums[l] + nums[r] # i번째 숫자 기준으로 합이 0이 되는 숫자 2개를 찾는 과정
+                    if s<0: # 마이너스 숫자가 너무 크니까 
+                        l += 1 # 오른쪽으로 이동
+                    elif s>0: # 플러스 숫자가 너무 크니까 
+                        r -= 1 # 왼쪽으로 이동
+                    else: # s = 0
+                        res.append((nums[i], nums[l], nums[r])) # 일치하는 숫자 찾으면 그거 넣어두고
+                        
+                        # 다른 숫자로 포인터 이동
+                        while l<r and nums[l] == nums[l+1]: 
+                            l += 1
+                        while l<r and nums[l] == nums[r-1]:
+                            r -= 1
+                        l += 1
+                        r -= 1
+        return res

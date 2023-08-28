5828490c09ae31ff7005f7939b7bb5faab659e49
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-08-28 12:14:52 +0900

    Time: 12 ms (74.44%), Space: 7.2 MB (30.27%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.c
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.c
@@ -1,14 +1,30 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        int l = 0, r= numbers.size()-1;
-        while (l<r){
-            if(numbers[l] + numbers[r] < target){
-                l++;
-            } else if (numbers[l] + numbers[r] > target){
-                r--;
-            } else {return {l+1,r+1};}
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* numbers, int numbersSize, int target, int* returnSize){
+    int *result;
+    int l, r, total;
+
+    result = malloc(2 * sizeof(int)); // int 2개를 return 할거니까 malloc으로 확보  
+    //assert(result);
+
+    *returnSize = 0;
+
+    l = 0;
+    r = numbersSize - 1;
+
+    while (l < r) {
+        total = numbers[l] + numbers[r];
+        if (total > target) {
+            r --;
+        } else if (total < target) {
+            l ++;
+        } else {
+            result[0] = l + 1; // index가 0 부터 시작하니까 몇번째인지는 +1
+            result[1] = r + 1; // index가 0 부터 시작하니까 몇번째인지는 +1
+            *returnSize = 2; // 이건 왜해주는거지?.. 모르겠다.
+            break;
         }
-        return {l+1,r+1};
     }
-};
+    return result;
+}

61da5287cb24727ae0d4c962a1b8b36c30aceb0f
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-08-30 11:31:21 +0900

    Time: 521 ms (59.98%), Space: 180.2 MB (38.00%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -14,10 +14,18 @@ public:
         ListNode* first = head;
         ListNode* second = head;
         
+        // 입력받는 k는 1부터 indexing이 돼서 
+        // k-2까지 해야해서 k-1로 지정해야함
+
+        // first가 k-1만큼 앞으로 이동
         for (int i=0; i<k-1;i++){
             first = first->next;
         }
+
+        // tail이 k-1만큼 움직인 first와 같은 위치에서 출발
         ListNode* tail = first;
+
+        // tail을 끝으로 보내버림 -> second는 head에서 출발했기 때문에 length - k 만큼 이동한 위치에 있게 됨.
         while(tail->next != nullptr){
             second = second->next;
             tail = tail->next;

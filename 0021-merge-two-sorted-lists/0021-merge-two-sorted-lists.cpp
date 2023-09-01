2ec6e62222e1f0199da329a6bf012c26fa60b418
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-09-01 21:22:53 +0900

    Time: 7 ms (61.96%), Space: 14.8 MB (65.10%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,28 +11,32 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* dummy = new ListNode(0);
-        ListNode* tail = dummy;
+        ListNode * dummy = new ListNode(0); // cpp에서는 new를 붙여서 새로운 노드를 만들어야함
+        ListNode * tail = dummy;
 
-        while (list1 && list2) {
-            if (list1->val <= list2->val) {
-                tail->next = list1;
-                list1 = list1->next;
-            } else {
+        // Until one of two list reached end
+        while (list1 and list2){
+            // Compare two values all the time
+            if (list1->val >= list2->val){
+                tail->next = list2;
+                // Move on to next node
+                //list2, tail = list2->next, list2;
                 tail->next = list2;
                 list2 = list2->next;
+
+            } else {
+                tail->next = list1;
+                //list1, tail = list1->next, list1;
+                list1 = list1->next;
             }
             tail = tail->next;
-        }
 
-        // Append the remaining elements from list1 or list2
-        if (list1) {
-            tail->next = list1;
-        } else {
-            tail->next = list2;
         }
-
+        // One element from one of two list will be remained
+        if (list1 or list2){
+            if (list1){tail->next = list1;}
+            else {tail->next = list2;}
+        }
         return dummy->next;
     }
 };
-

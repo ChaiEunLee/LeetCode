2a94dbbe278c06837d187a6b0cd252fab6fd260d
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-09-01 21:30:32 +0900

    Time: 2 ms (79.18%), Space: 6.2 MB (36.76%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
@@ -5,30 +5,35 @@
  *     struct ListNode *next;
  * };
  */
-struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
-    struct ListNode* dummy = (struct ListNode*)malloc(sizeof(struct ListNode));
-    struct ListNode* tail = dummy;
 
-    // Until one of two lists reaches the end
-    while (list1 && list2) {
-        if (list1->val <= list2->val) {
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
+    struct ListNode * dummy = (struct ListNode*)malloc(sizeof(struct ListNode)); // C는 malloc으로 새로운 노드 만들어야함.
+    struct ListNode * tail = dummy;
+
+        // Until one of two list reached end
+        while (list1 && list2){
+            // Compare two values all the time
+            if (list1->val >= list2->val){
+                tail->next = list2;
+                // Move on to next node
+                //list2, tail = list2->next, list2;
+                list2 = list2->next;
+
+            } else {
+                tail->next = list1;
+                //list1, tail = list1->next, list1;
+                list1 = list1->next;
+            }
+            tail = tail->next;
+
+        }
+        // Append the remaining elements from list1 or list2
+        if (list1) {
             tail->next = list1;
-            list1 = list1->next;
         } else {
             tail->next = list2;
-            list2 = list2->next;
         }
-        tail = tail->next;
+        struct ListNode* result = dummy->next;
+        free(dummy); // Free the dummy node since it's no longer needed
+        return result;
     }
-
-    // Append the remaining elements from list1 or list2
-    if (list1) {
-        tail->next = list1;
-    } else {
-        tail->next = list2;
-    }
-
-    struct ListNode* result = dummy->next;
-    free(dummy); // Free the dummy node since it's no longer needed
-    return result;
-}

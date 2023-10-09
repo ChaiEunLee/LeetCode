bfc2aec7ad2fe406e7f1036f20d896c84bbef55b
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-10-09 10:41:13 +0900

    Time: 38 ms (73.34%), Space: 16.4 MB (16.43%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,12 +1,9 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        number = 0
         for i in range(len(digits)-1,-1,-1):
-            number += digits[i] * (10**(len(digits)-i-1))
-        answer = []
-        number += 1
-        
-        while number > 0:
-            answer = [number%10] + answer
-            number = number // 10 
-        return answer
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] = digits[i] + 1
+                return digits #제일 왼쪽은 따로 더 있는 경우
+        return [1] + digits #제일 왼쪽이 1인 경우

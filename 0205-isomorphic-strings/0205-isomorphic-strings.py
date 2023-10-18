013fc4bb6d5a3ccb979413f27093e8043baeda71
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-10-18 09:50:56 +0900

    Time: 51 ms (31.52%), Space: 16.5 MB (88.29%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,10 +1,16 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        m1, m2 = [0] * 256, [0] * 256
-        n = len(s)
-        for i in range(n):
-            if m1[ord(s[i])] != m2[ord(t[i])]:
-                return False
-            m1[ord(s[i])] = i + 1
-            m2[ord(t[i])] = i + 1
+        
+        # s="egg", t="add"일때,
+        d = {}
+        for i, j in zip(s, t): # e,a / g,d / g,d 
+            if i not in d: #e가 key, a가 value로 들어갈건데,
+                if j in d.values(): # 이미 e랑 매핑된 다른 알파벳이 있으면
+                    return False # return False
+                d[i] = j # 'e':'a'로 매핑
+
+            else:
+                if d[i] != j:
+                    return False
+
         return True

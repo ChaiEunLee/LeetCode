03c545d8ee8ccb61410fc25079c9fe965dc77f99
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-11-16 09:38:26 +0900

    Time: 43 ms (21.73%), Space: 16.3 MB (16.53%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,60 +1,29 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        # 지나가면 0으로 만들어버림.
-        # len()나 0을 마주하면 방향틀기
-
+        if not matrix:
+            return []
         
-        spiral = []
-        i,j = 0,0
-        col = True
-        increase = True
-        # and 를 넣을떄는 순서가 중요함
-        while j<len(matrix[0]) and i<len(matrix) and matrix[i][j] != 101:
-            #print(i,j,matrix[i][j],col,increase)
-            spiral.append(matrix[i][j])
-            matrix[i][j] = 101
+        rows, cols = len(matrix), len(matrix[0])
+        top, bottom, left, right = 0, rows-1, 0, cols-1
+        result = []
+        
+        while len(result) < rows*cols:
+            for i in range(left, right+1):
+                result.append(matrix[top][i])
+            top += 1
             
-            # i,j 길이도 보고 
-            # matrix[i][j]==0이면 
-            # col 이동
-            if col:
-                if increase: 
-                    j+= 1
-                else: 
-                    j -= 1
-                    
-                if j==len(matrix[0]) or matrix[i][j]==101:
-                    #row 이동 + 부호는 그대로
-                    # j 원상복귀하고 
-                    if increase:
-                        j -= 1  
-                    else:
-                        j += 1
-                    col = False
-                    if increase: 
-                        i +=1 
-                    else: 
-                        i-=1
-            else:
-                if increase: 
-                    i+= 1
-                else: 
-                    i -= 1
-                if i==len(matrix) or matrix[i][j]==101:
-                    #col 이동 + 무조건 부호도 바뀌어야함.
-                    # i 원상복귀하고 
-                    if increase:
-                        i -= 1  
-                    else:
-                        i += 1
-                    col = True
-                    increase = not increase
-                    if increase:
-                        j += 1  
-                    else:
-                        j -= 1
-            #print(spiral,i,j)
-        
-        return spiral
+            for i in range(top, bottom+1):
+                result.append(matrix[i][right])
+            right -= 1
+            
+            if top <= bottom:
+                for i in range(right, left-1,-1):
+                    result.append(matrix[bottom][i])
+                bottom -= 1
 
-                
+            if left <= right:
+                for i in range(bottom, top-1,-1):
+                    result.append(matrix[i][left])
+                left += 1
+            
+        return result

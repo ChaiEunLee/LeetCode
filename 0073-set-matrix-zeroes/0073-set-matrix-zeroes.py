21fca7af4bb91f3a27acfa692d7b3bbd4110fa62
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-11-20 09:58:57 +0900

    Time: 120 ms (37.18%), Space: 17.1 MB (59.95%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,26 +1,32 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        if not matrix:
-            return []
-        
+
         m = len(matrix)
         n = len(matrix[0])
+		
+        first_row_has_zero = False
+        first_col_has_zero = False
         
-        zeroes_row = [False] * m
-        zeroes_col = [False] * n
-        
-        # 0이 있는 row, col을 기록
+        # iterate through matrix to mark the zero row and cols
         for row in range(m):
             for col in range(n):
                 if matrix[row][col] == 0:
-                    zeroes_row[row] = True
-                    zeroes_col[col] = True
-                    
-        # 0이 있는 row, col을 다 0으로 만듦
-        for row in range(m):
+                    if row == 0:
+                        first_row_has_zero = True
+                    if col == 0:
+                        first_col_has_zero = True
+                    matrix[row][0] = matrix[0][col] = 0
+    
+        # iterate through matrix to update the cell to be zero if it's in a zero row or col
+        for row in range(1, m):
+            for col in range(1, n):
+                matrix[row][col] = 0 if matrix[0][col] == 0 or matrix[row][0] == 0 else matrix[row][col]
+        
+        # update the first row and col if they're zero
+        if first_row_has_zero:
             for col in range(n):
-                if zeroes_row[row] or zeroes_col[col]:
-                    matrix[row][col] = 0
+                matrix[0][col] = 0
+        
+        if first_col_has_zero:
+            for row in range(m):
+                matrix[row][0] = 0

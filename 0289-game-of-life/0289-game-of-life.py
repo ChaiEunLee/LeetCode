3918191db0362cb743d35c6b785139f76f4b5474
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-11-21 11:16:04 +0900

    Time: 36 ms (81.75%), Space: 16.5 MB (8.11%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.py
+++ 0289-game-of-life/0289-game-of-life.py
@@ -1,10 +1,14 @@
 class Solution:
+    # board[I][J]에는 1이 몇개인지 세어주는 함수
     def gameOfLifeInfinite(self, live):
+        # live한 index들이 들어왔으니까 그거 기준으로 개수셈
         ctr = collections.Counter((I,J)
                                for i,j in live
                                 for I in range(i-1,i+2)
                                 for J in range(j-1, j+2)
                                 if I != i or J != j)
+        
+        # 그중에 다음 step에서도 live한 index들만 return
         return {ij
                 for ij in ctr
                 if ctr[ij] == 3 or ctr[ij]==2 and ij in live}
@@ -22,7 +26,11 @@ class Solution:
         # i, row <- index, row를 출력
         # j, live <- index, 각 row 값 하나씩 출력
         # if live : 출력한 값이 1인 index만 live에 들어감.
+        
+        # 다음 step에서 live 인것들 받음        
         live = self.gameOfLifeInfinite(live)
+        
+        # 다음 step에서 live인거 적용
         for i, row in enumerate(board):
             for j in range(len(row)):
                 row[j] = int((i,j) in live)

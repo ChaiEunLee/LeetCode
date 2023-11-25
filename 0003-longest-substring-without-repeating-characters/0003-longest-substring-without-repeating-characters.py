608a4bd5e4b41cafb4f92e2505040ed464eab4f4
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-11-25 15:04:40 +0900

    Time: 54 ms (85.33%), Space: 16.5 MB (19.32%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,17 +1,17 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # 길이를 1~len(s)/2로 늘려가면서 
-        # 한칸씩 이동하면서 봐야하는데 
-        sub = ""
-        maxlength = 0
-        for i in range(len(s)): #시작지점
-            for j in range(i,len(s)): #시작지점부터 끝까지 돌면서
-                if s[j] in sub: # repeating letter 나오면
-                    sub = "" 
-                    break     
-                sub += s[j] # repeating letter 안 나올 때까지 더해주고 
-                if maxlength < len(sub): # maxlength 업데이트
-                    maxlength = len(sub)
-                #print(sub, maxlength)
-        return maxlength
-            
+        seen = {} # 해당 letter가 가장 최근에 몇번째에 나왔는지를 dictionary로 기록
+        l = 0 # 시작지점
+        output = 0 # 길이
+        for r in range(len(s)):
+#            print(s[r], seen, l, output)
+            if s[r] not in seen:
+                output = max(output, r-l+1)
+            else:
+                if seen[s[r]] < l:
+                    output = max(output, r-l+1) # r-l+1 = 현재위치-시작위치+1로 길이 측정
+                else:
+                    l = seen[s[r]] + 1 # 그 letter가 나왔던 그 위치 다음으로 시작 위치를 지정. 
+            seen[s[r]] = r # seen[s[r]]을 현재 위치로 업데이트
+        return output
+                

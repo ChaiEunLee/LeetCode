e003613dea6ab350fdf6fb496b56d2b07733a919
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-12-27 10:44:03 +0900

    Time: 289 ms (99.01%), Space: 28 MB (99.90%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,21 +5,16 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        rev = None
         slow = fast = head
         
-        while fast and fast.next:
+        while fast and fast.next: #fast.next를 꼭 넣어줘야함. 
             fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next #slow를 움직이면서 reverse list로 동시에 만들어줌.
+        if fast:
             slow = slow.next
-        
-        #init
-        prev, slow, prev.next = slow, slow.next, None
-        
-        while slow:
-            slow.next, prev, slow = prev, slow, slow.next
-        
-        fast, slow = head, prev
-        while slow:
-            if fast.val != slow.val:
-                return False
-            fast, slow = fast.next, slow.next
-        return True        
+            
+        # Check palindrome
+        while rev and rev.val == slow.val:
+            slow, rev = slow.next, rev.next
+        return not rev

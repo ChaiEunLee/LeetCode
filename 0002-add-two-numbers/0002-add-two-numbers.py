1bd6a6a6ba642503fc6e7bb97fb3e056bd3c16e6
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-12-28 10:59:26 +0900

    Time: 73 ms (15.98%), Space: 17.3 MB (5.06%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,27 +5,48 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        carry = 0
-        root = n = ListNode(0) #나중에 새로운 list의 맨 앞(root.next)을 반환해야해서
-        while l1 or l2 or carry: #carry만 남을 수도 있으니까
-            v1 = v2 = 0
+        # reverse -> add -> reverse
+        
+        # 자릿수 법칙을 좀 잘 사용해야할듯.
+        # reverse order로 저장되어있으니까 앞부터 더하면 됨.
+        carrier = 0
+        sumlisthead = sumlist = ListNode()
+        
+        while l1 and l2:
+            num = l1.val + l2.val + carrier
+            if num >= 10:
+                carrier = 1
+                num %= 10
+            else:
+                carrier = 0
+            sumlist.next = ListNode(num)
+            sumlist = sumlist.next
+            l1 = l1.next
+            l2 = l2.next
+        
+        # 리스트가 남으면
+        while l1 or l2:
+            # which list is left : l1 or l2
             if l1:
-                v1 = l1.val
+                num = l1.val + carrier
                 l1 = l1.next
             if l2:
-                v2 = l2.val
+                num = l2.val + carrier
                 l2 = l2.next
-               
-            #carry, v = divmod((v1+v2+carry), 10)
-            # val부터 해야지 carry부터 계산하면 안 됨!
-            v = (v1+v2+carry) % 10 #일의자리
-            carry = (v1+v2+carry) // 10 #십의자리
             
-                        
-            # n이 first node고 n.next에 계속 하나씩 넣으면서 앞에다가 붙여주는 형식
-            n.next = ListNode(v)
-            n = n.next
+            # carrier
+            if num >= 10:
+                carrier = 1
+                num %= 10
+            else:
+                carrier = 0
+                
+            sumlist.next = ListNode(num)
+            sumlist = sumlist.next
         
-        return root.next
-    
-    
+        # if carrier = 1
+        if carrier == 1:
+            sumlist.next = ListNode(1)
+            
+        return sumlisthead.next
+        

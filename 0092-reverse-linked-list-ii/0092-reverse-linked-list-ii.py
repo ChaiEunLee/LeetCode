9212f263005aa1c2ac2b86194a46097b2e19801c
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-12-29 11:51:48 +0900

    Time: 78 ms (5.82%), Space: 17.4 MB (6.75%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -5,20 +5,24 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        # 예외
         if not head or left==right:
             return head
-        p = dummy = ListNode()
-        dummy.next = head
         
+        # start : 변경이 필요한 left의 바로 앞 지점
+        # end : start.next
+        # root : head보다 앞에 root를 만듦. 최종 결과는 root.next로 return 할 예정.
+        root = start = ListNode(None)
+        root.next = head
+        # -> 이렇게 할당된 start, end는 끝까지 값이 변하지 않음. end는 reverse 부분의 맨 마지막이 될테니. 
+        # start, end 지정
         for _ in range(left-1):
-            p = p.next
-        tail = p.next
+            start = start.next
+        end = start.next
         
+        # 반복하면서 노드 차례대로 뒤집기
         for _ in range(right-left):
-            tmp = p.next
-            p.next = tail.next
-            tail.next = tail.next.next
-            p.next.next = tmp
-        
-        return dummy.next
-        
+            tmp, start.next, end.next = start.next, end.next, end.next.next
+            start.next.next = tmp
+            
+        return root. next

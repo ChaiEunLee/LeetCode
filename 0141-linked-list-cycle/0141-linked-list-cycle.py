40b73d3fa7bbb74b884a936e81bc296be4c74fac
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-12-29 13:51:20 +0900

    Time: 117 ms (5.87%), Space: 19.9 MB (93.49%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,13 +6,15 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow = fast = head
-
-        while fast and fast.next:             
-            slow = slow.next
-            fast = fast.next.next
-
-            if slow == fast:
-                return True
-
-        return False        
+        # slow, fast를 사용하면 cycle이 있다면 언젠간 만난다는 점을 이용
+        try:
+            slow = head
+            fast = head.next
+            while slow is not fast: 
+                # cycle이 있다면 언젠간 만남.
+                slow = slow.next
+                fast = fast.next.next
+            return True
+        except: 
+            #slow/fast가 끝까지 가서 slow.next, fast.next.next이 없으면 에러가 나면서 except로 가게됨. cycle이 없다는 뜻
+            return False

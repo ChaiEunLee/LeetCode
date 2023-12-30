c50cd84abfac6ee02a2ac03aecca9605597bbf86
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2023-12-30 12:09:58 +0900

    Time: 60 ms (5.88%), Space: 17.3 MB (7.31%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,19 +1,11 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        myDict = {'(':')', '{':'}', '[':']'}
-        queue = []
-        for i in range(len(s)):
-            if s[i] in ['(','{','[']:
-                queue.append(s[i])
-            else:
-                if queue:
-                    tmp = queue.pop()
-                else: 
-                    return False
-                if s[i] != myDict[tmp]:
-                    return False
-        # 괄호가 남아있으면
-        if queue:
-            return False
-        return True
+        stack = []
+        myDict = {')':'(', '}':'{', ']':'['} #괄호 key, value를 거꾸로
+        for char in s:
+            if char not in myDict: #dictioanry key에 해당하는걸 이렇게 거를수 있음
+                stack.append(char)
+            elif not stack or myDict[char] != stack.pop():
+                return False
+        return len(stack)==0 #괄호 남아있으면 false
             

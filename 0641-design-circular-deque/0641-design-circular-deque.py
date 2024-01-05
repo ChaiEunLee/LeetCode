898a49b78b04617892bfb69635f98f20982f7a13
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-01-05 10:30:40 +0900

    Time: 59 ms (92.84%), Space: 18.1 MB (14.02%) - LeetHub

--- 0641-design-circular-deque/0641-design-circular-deque.py
+++ 0641-design-circular-deque/0641-design-circular-deque.py
@@ -1,21 +1,12 @@
 class MyCircularDeque:
 
-    def __init__(self, k):
-        """
-        Initialize your data structure here. Set the size of the deque to be k.
-        :type k: int
-        """
+    def __init__(self, k: int):
         self._size = 0
-        self._front, self._rear = 0, 0
+        self._front, self._rear = 0,0
         self._capacity = k
-        self._data = [-1] * k
+        self._data = [-1]*k
 
-    def insertFront(self, value):
-        """
-        Adds an item at the front of Deque. Return true if the operation is successful.
-        :type value: int
-        :rtype: bool
-        """
+    def insertFront(self, value: int) -> bool:
         if self.isFull():
             return False
         if self.isEmpty():
@@ -26,41 +17,29 @@ class MyCircularDeque:
         self._size += 1
         return True
 
-    def insertLast(self, value):
-        """
-        Adds an item at the rear of Deque. Return true if the operation is successful.
-        :type value: int
-        :rtype: bool
-        """
+    def insertLast(self, value: int) -> bool:
         if self.isFull():
             return False
-        if self.isEmpty():
+        if self.isEmpty(): #처음엔 ._rear의 value가 -1이니까. 그 다음부터는 값이 있어서 조건문으로 따로 해주는 것.
             self._data[self._rear] = value
         else:
-            self._rear = (self._rear + 1) % self._capacity
+            self._rear = (self._rear+1) % self._capacity
             self._data[self._rear] = value
         self._size += 1
         return True
+        
 
-    def deleteFront(self):
-        """
-        Deletes an item from the front of Deque. Return true if the operation is successful.
-        :rtype: bool
-        """
+    def deleteFront(self) -> bool:
         if self.isEmpty():
             return False
         self._data[self._front] = -1
         self._front = (self._front + 1) % self._capacity
         self._size -= 1
         if self.isEmpty():
-            self._rear = self._front
+            self._front = self._rear
         return True
 
-    def deleteLast(self):
-        """
-        Deletes an item from the rear of Deque. Return true if the operation is successful.
-        :rtype: bool
-        """
+    def deleteLast(self) -> bool:
         if self.isEmpty():
             return False
         self._data[self._rear] = -1
@@ -69,31 +48,27 @@ class MyCircularDeque:
         if self.isEmpty():
             self._front = self._rear
         return True
-
-    def getFront(self):
-        """
-        Get the front item from the deque.
-        :rtype: int
-        """
+        
+    def getFront(self) -> int:
         return self._data[self._front]
 
-    def getRear(self):
-        """
-        Get the last item from the deque.
-        :rtype: int
-        """
+    def getRear(self) -> int:
         return self._data[self._rear]
 
-    def isEmpty(self):
-        """
-        Checks whether the circular deque is empty or not.
-        :rtype: bool
-        """
-        return self._size == 0
+    def isEmpty(self) -> bool:
+        return self._size == 0        
 
-    def isFull(self):
-        """
-        Checks whether the circular deque is full or not.
-        :rtype: bool
-        """
+    def isFull(self) -> bool:
         return self._size == self._capacity
+
+
+# Your MyCircularDeque object will be instantiated and called as such:
+# obj = MyCircularDeque(k)
+# param_1 = obj.insertFront(value)
+# param_2 = obj.insertLast(value)
+# param_3 = obj.deleteFront()
+# param_4 = obj.deleteLast()
+# param_5 = obj.getFront()
+# param_6 = obj.getRear()
+# param_7 = obj.isEmpty()
+# param_8 = obj.isFull()

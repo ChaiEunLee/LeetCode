67ffb1b0094b6215b623bd04ee5aca0a57ca7e3b
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-01-06 17:13:06 +0900

    Time: 267 ms (80.91%), Space: 20.5 MB (46.02%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,24 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        #base
         if not grid:
-            return 0
+            return False
         
-        visited = [[False for i in range(len(grid[0]))] for j in range(len(grid))]
-        island = 0
+        visited = [[False for _ in range(len(grid[0]))] for _ in range(len(grid))]
         
-        def dfs(i, j):
-            if 0<=i<len(grid) and 0<=j<len(grid[0]) and grid[i][j] == "1" and visited[i][j] == False:
-                visited[i][j] = True
-                dfs(i-1, j)
-                dfs(i+1, j)
-                dfs(i, j-1)
-                dfs(i, j+1)
-        
+        def dfs(x,y):
+            if 0<=x<len(grid) and 0<=y<len(grid[0]) and visited[x][y] == False and grid[x][y] == '1':
+                visited[x][y] = True
+                for nx, ny in (x-1,y),(x+1,y),(x,y-1),(x,y+1):
+                    dfs(nx,ny)
+
+                    
+        numisland = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == "1" and visited[i][j] == False:
-                    dfs(i, j)
-                    island += 1
+                if visited[i][j] is False and grid[i][j] == '1':
+                    dfs(i,j)
+                    numisland += 1
 
-        return island
+        return numisland

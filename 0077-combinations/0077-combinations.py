0fdd64533db1ef24fe8817ff96f8ec17fa49a609
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-01-08 10:37:12 +0900

    Time: 573 ms (99.69%), Space: 62.8 MB (94.74%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,18 +1,4 @@
+from itertools import combinations
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        results = []
-        
-        def dfs(elements, start:int, k:int):
-            if k==0:
-                results.append(elements[:])
-                return
-            
-            # 자신 이전의 모든 값을 고정하여 재귀 호출
-            # 자기 이전의 값을 고정하는게 포인트인듯!
-            for i in range(start, n+1):
-                elements.append(i)
-                dfs(elements, i+1, k-1)
-                elements.pop()
-                
-        dfs([], 1, k)
-        return results
+        return list(itertools.combinations(range(1,n+1),k))

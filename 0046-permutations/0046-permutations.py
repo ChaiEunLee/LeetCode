cf07391fc75a0f3e233c39a7c82ef49617d51e81
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-01-08 10:07:23 +0900

    Time: 47 ms (46.31%), Space: 17.4 MB (28.63%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,22 +1,5 @@
+from itertools import permutations
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        results = []
-        prev_elements = []
+        return list(itertools.permutations(nums))
         
-        def dfs(elements):
-            # add result when it is leaf node
-            if len(elements) == 0:
-                # result에 추가 
-                results.append(prev_elements[:]) #copy()를 하지 않으면 reference가 됨.                 
-                
-            for e in elements:
-                next_elements = elements[:]
-                next_elements.remove(e)
-                
-                prev_elements.append(e)
-                dfs(next_elements)
-                prev_elements.pop()               
-                
-        # Run dfs
-        dfs(nums)
-        return results

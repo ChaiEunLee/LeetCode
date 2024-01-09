128b36ed4d8aa69e5920a893f81baa7009c6cd12
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-01-09 13:22:09 +0900

    Time: 71 ms (95.02%), Space: 17.4 MB (35.96%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -6,19 +6,18 @@ class Solution:
         graph = collections.defaultdict(list)
         
         # 그래프를 뒤집어서 구성
-        for a, b in sorted(tickets, reverse=True):
+        for a, b in sorted(tickets):
             graph[a].append(b)
         #print(graph)
         
-        route = []
-        def dfs(a):
-            # 마지막 값을 읽어 어휘 순 방문
-            while graph[a]:
-                dfs(graph[a].pop())
-            route.append(a)
-            #print(a,route)
-            
-        dfs('JFK')
-        # 다지 뒤집에서 어휘 순 결과로
-        return route[::-1]
+        # stack에서 하나씩 꺼내서 route를 채우는 개념 
+        route, stack = [], ['JFK']
+        while stack:
+            # 반복으로 스택을 구성하되 막히는 부분에서 풀어내는 처리
+            while graph[stack[-1]]:
+                stack.append(graph[stack[-1]].pop(0))
+            route.append(stack.pop())
+            #print(route, stack)
         
+        # 다시 뒤집어 어휘 순 결과로
+        return route[::-1]

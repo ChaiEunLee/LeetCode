b845478ebb8930b08ceee08e388578ac27ca2ac7
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-01-17 10:14:19 +0900

    Time: 41 ms (75.29%), Space: 17.5 MB (17.76%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,5 +1,16 @@
-from itertools import permutations
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return list(itertools.permutations(nums))
+        #for문 전체를 두번 돌면서
         
+        def dfs(per):
+            if len(per)==len(nums):
+                answer.append(per)
+                return 
+            for k in range(len(nums)): 
+                if nums[k] not in per:
+                    dfs(per+[nums[k]])
+
+        answer = []
+        for i in range(len(nums)):
+            dfs([nums[i]])
+        return answer

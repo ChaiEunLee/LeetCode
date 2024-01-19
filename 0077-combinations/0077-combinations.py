d852cf7cc7455f47c8c2ac4d3f0a7cca9c50a1a4
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-01-19 09:58:03 +0900

    Time: 744 ms (73.33%), Space: 65.6 MB (46.70%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,4 +1,19 @@
-from itertools import combinations
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        return list(itertools.combinations(range(1,n+1),k))
+        
+        # bfs
+        def bfs(i, comb):
+            if len(comb) == k:
+                answer.append(comb)
+                return
+            
+            # i+1~n까지
+            for j in range(i+1,n+1):
+                bfs(j,comb+[j])
+            
+        answer = []
+        # 0~n-k까지인데 index가 1부터라 1~n-k+1까지여야해서 range(1,n-k+2)
+        for i in range(1,n-k+2): 
+            bfs(i,[i])
+            
+        return answer

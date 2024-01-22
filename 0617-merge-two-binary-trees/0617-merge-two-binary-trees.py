583b6c503236c1bcd2da2a06154a78da6f9b95f3
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-01-22 10:06:19 +0900

    Time: 64 ms (75.04%), Space: 17 MB (99.66%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
@@ -1,17 +1,30 @@
 # Definition for a binary tree node.
-#class TreeNode:
-#    def __init__(self, val=0, left=None, right=None):
-#        self.val = val
-#        self.left = left
-#        self.right = right
-
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-        if root1 and root2:        
-            root = TreeNode(root1.val+root2.val)
-            root.left = self.mergeTrees(root1.left, root2.left)
-            root.right = self.mergeTrees(root1.right, root2.right)
-            return root
-        else:
-            return root1 or root2
-    
+        # 첫 input이 root2만 들어올 때
+        if not root1:
+            return root2
+        if not root2:
+            return root1
+        
+        # start
+        root1.val = root1.val + root2.val
+        
+        if root1.left and root2.left:
+            self.mergeTrees(root1.left, root2.left)
+        elif root2.left:
+            root1.left = root2.left
+
+        
+        if root1.right and root2.right:
+            self.mergeTrees(root1.right, root2.right)
+        elif root2.right:
+            root1.right = root2.right
+
+        return root1
+        

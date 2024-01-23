7a7eede5890c96a11f0bf14c5be98bb3db3028c7
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-01-23 09:40:11 +0900

    Time: 440 ms (59.24%), Space: 27.1 MB (86.91%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -1,23 +1,35 @@
+import collections
+
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        # base case
-        if n==1: return [0]
-        # adj 리스트를 만들어서 graph 형태로 만들어줌.
-        adj = [set() for _ in range(n)]
+        # 리프 노드를 하나씩 제거하면서 남아있는 값을 찾으면 가운데에 있는 노드이고 이걸 루트로 하면 최소 높이 구성이 가능함. 
+        # 루트 찾기 -> 그걸로 트리 구성하기 
+        
+        if n<=1:
+            return [0]
+        
+        # 양방향 graph 구성
+        graph = collections.defaultdict(list)
         for i,j in edges:
-            adj[i].add(j)
-            adj[j].add(i)
-        # leaves를 만들어서 하나씩 제거할 예정
-        leaves = [i for i in range(n) if len(adj[i]) == 1]
-        # 각 leave와 연결된 node를 제거, 그 node에 가서 leaves도 제거. - edge를 소거함.
+            graph[i].append(j)
+            graph[j].append(i)
+        
+        # 연결된 노드가 하나면 그 노드는 leaf nodes
+        leaves = []
+        for i in range(n+1):
+            if len(graph[i]) == 1:
+                leaves.append(i)
+        
+        # 1) 루프 노드 찾기                
         while n>2:
-            n -= len(leaves) #leaves는 어차피 root가 될 수 없으니까 root 후보에서 제외
-            newLeaves = []
-            for i in leaves: 
-                j = adj[i].pop() #j = 각 leave와 연결된 vertex
-                adj[j].remove(i) # 그 leaves와 연결된 vertex에 가서 leaves를 제거
-                if len(adj[j])==1:
-                    newLeaves.append(j)
-            leaves = newLeaves
-        # 그러다가 edge가 1,2개 남으면 만나는 것.
+            n -= len(leaves)
+            new_leaves = []
+            for leaf in leaves:
+                neighbor = graph[leaf].pop()
+                graph[neighbor].remove(leaf)
+                
+                if len(graph[neighbor])==1:
+                    new_leaves.append(neighbor)
+            leaves = new_leaves
+            #print(leaves)
         return leaves

4c7d8de589e1cac241c068a7dd053c127046bafd
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-01-29 08:21:50 +0900

    Time: 188 ms (64.65%), Space: 19.3 MB (89.99%) - LeetHub

--- 0687-longest-univalue-path/0687-longest-univalue-path.py
+++ 0687-longest-univalue-path/0687-longest-univalue-path.py
@@ -5,18 +5,29 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    result = 0
     def longestUnivaluePath(self, root: Optional[TreeNode]) -> int:
-        longest = [0]
-        def traverse(node):
-            if not node:
+        # DFS로 내려가다가 leaf에 도달하면 그때부터 backtracking으로 거리를 누적해 계산하는 방식
+        def dfs(node):
+            if node is None:
                 return 0
-            left_len, right_len = traverse(node.left), traverse(node.right)
-            left = (left_len+1) if node.left and node.left.val == node.val else 0
-            right = (right_len+1) if node.right and node.right.val == node.val else 0
-            longest[0] = max(longest[0], left+right)
-            #print(node.val, longest, left, right)
-            return max(left, right)
             
-        traverse(root)
-        return longest[0]
-        
+            # 존재하지 않는 노드까지 DFS 재귀 탐색. leaf에 도달하면 0을 return. 
+            left = dfs(node.left) 
+            right = dfs(node.right)
+            
+            # 현재 노드가 자식 노드와 동일한 경우 거리 1 증가
+            if node.left and node.left.val == node.val:
+                left += 1
+            else:
+                left = 0
+            if node.right and node.right.val == node.val:
+                right += 1
+            else:
+                right = 0
+                
+            # 왼쪽과 오른쪽 노드 간 거리의 합 최댓값이 결과
+            self.result = max(self.result, left+right)
+            return max(left, right)
+        dfs(root)
+        return self.result

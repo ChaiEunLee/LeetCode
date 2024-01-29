15be4fddc24f66bd3070d5dc33a69f4432d640a2
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-01-30 08:09:20 +0900

    Time: 41 ms (40.18%), Space: 16.5 MB (73.18%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -4,16 +4,14 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
-
 class Solution:
-    def rightSideView(self, root):
-        view = []
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        # level 만들고 -1 return
+        answer = []
         if root:
             level = [root]
             while level:
-                #level에 순서대로 넣으니까 가장 오른쪽에 있는 node값을 view에 넣는다.
-                view += level[-1].val,
-                #각 level에 있는 노드별로 left, right가 있으면 그걸 level로 업데이트. 
+                vals = [node.val for node in level]
+                answer.append(vals[-1])
                 level = [kid for node in level for kid in (node.left, node.right) if kid]
-        return view
+        return answer

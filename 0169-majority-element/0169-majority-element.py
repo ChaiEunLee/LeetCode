5db8c9e44baee05cf6e3dcceaa46b7625576dbe5
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-02-03 09:31:05 +0900

    Time: 9753 ms (5.01%), Space: 18 MB (60.34%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,13 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        myDict = {}
-        for n in nums:
-            if n in myDict:
-                myDict[n] += 1
-            else:
-                myDict[n] = 1
-        
-        # list 길이가 1인 경우가 있을수 있어서
-        for key, value in myDict.items():
-            if value > len(nums)//2:
-                return key
+        for num in nums:
+            if nums.count(num) > len(nums)//2:
+                return num
+            

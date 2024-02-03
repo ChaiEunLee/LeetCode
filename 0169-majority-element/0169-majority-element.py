2f47a8c40d1c39df3c83f51a38f6d631a63dddb2
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-02-03 09:38:59 +0900

    Time: 226 ms (7.39%), Space: 18.1 MB (51.76%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,10 +1,15 @@
 import collections
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        counts = collections.defaultdict(int)
-        for num in nums:
-            if counts[num]==0:
-                counts[num] = nums.count(num)
-            
-            if counts[num] > len(nums)//2:
-                return num
+        # 분할한 부분에서 과반을 찾아서 계속 위로 올려보내는 방식
+        if not nums:
+            return None
+        if len(nums) == 1:
+            return nums[0]
+        
+        half = len(nums)//2
+        a = self.majorityElement(nums[:half])
+        b = self.majorityElement(nums[half:])
+        print([b,a])
+        return [b,a][nums.count(a)>half]
+        

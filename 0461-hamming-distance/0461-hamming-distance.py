4617ee968672738ae710f99a2ecc1e4e555483f3
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-02-16 10:43:24 +0900

    Time: 38 ms (43.64%), Space: 16.3 MB (97.24%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.py
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -2,6 +2,10 @@ class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
         # bin : 10진수->2진수로 바꾸는 함수
         # x^y 하면 알아서 2진수 XOR -> 10진수로 바꿔줌
-        #print(x^y)
-        #print(bin(x^y))
-        return bin(x^y).count('1')
+        xor = x^y
+        count=0
+        for _ in range(32):
+#            print(xor, xor&1)
+            count += xor & 1 #맨 뒷자리가 1인것만 count 하겠다는것(xor 결과가 1이면 다른거라서)
+            xor = xor>>1 #shift
+        return count

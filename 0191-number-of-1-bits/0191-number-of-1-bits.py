7621c9d90a02d951426c0756761fbafd45cbf7d9
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-02-18 12:29:29 +0900

    Time: 42 ms (24.14%), Space: 16.6 MB (53.18%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -1,3 +1,8 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        return bin(n).count('1')
+        count = 0
+        while n:
+            print(n, count)
+            n &= n-1 # 1을 뺀 값과 AND 연산을 할 때마다 비트가 1씩 빠지게 됨. 0이 될 때까지 반복하면 1의 개수를 세게 됨.
+            count += 1
+        return count

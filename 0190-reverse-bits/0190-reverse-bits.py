fc11f7116485b4913a4bf498d0f811113d9e1804
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-02-19 09:06:44 +0900

    Time: 47 ms (11.44%), Space: 16.5 MB (83.99%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,9 +1,10 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        result = 0
-        bit = 31
-        for i in range(len(bin(n))-1,1,-1): #n-1~2까지
-            result += 2**(bit)*int(bin(n)[i])
-            bit -= 1
-            #print(i, result, bit)
-        return result
+        ans = 0
+        for i in range(32):
+            ans = (ans<<1) + (n&1) 
+            #n&1 맨 오른쪽bit가 1이면 1, 0이면 0이 나옴.
+            # ans<<1 하고 + 위에서 구한 0/1을 오른쪽에 붙임. 
+            n>>=1 # n 한칸 right shift해서 맨 오른쪽 자릿수 없앰. 
+            #print(bin(n), bin(ans))
+        return ans

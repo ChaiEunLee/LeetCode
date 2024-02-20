e26d83f1ded9d6da81bfd7bc72bae508568f6e32
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-02-20 09:53:21 +0900

    Time: 37 ms (65.48%), Space: 16.6 MB (62.46%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,5 +1,14 @@
 class Solution:
+    dp = collections.defaultdict(int)
     def fib(self, n: int) -> int:
+
         if n<=1:
             return n
-        return self.fib(n-1)+self.fib(n-2)
+        # 앞에서 계산했던 값이라면 그 값을 기억해놨다가 return 
+        if self.dp[n]:
+            return self.dp[n]
+
+        self.dp[n] = self.fib(n-1) + self.fib(n-2)
+        return self.dp[n]
+        
+        

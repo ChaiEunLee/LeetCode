0f9778d728dee24a2335c8ddae980a8b093941a9
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-02-21 10:52:52 +0900

    Time: 32 ms (77.39%), Space: 16.5 MB (76.94%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,13 +1,9 @@
 class Solution:
+    dp = collections.defaultdict(int)
     def climbStairs(self, n: int) -> int:
-        # how many distinct ways 
-        # step list 만들고 각 step 별로 숫자 카운트
-        if n==1:
-            return 1
-        step = [0]*(n+1)
-        step[1] = 1
-        step[2] = 1
-        for i in range(2,n+1):
-            step[i] = step[i]+step[i-1]+step[i-2]
-            print(step)
-        return step[n]
+        if n<=2:
+            return n
+        if self.dp[n]:
+            return self.dp[n]
+        self.dp[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        return self.dp[n]

b0a254540450c2c39142f648b2e6a23d65533040
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-02-21 10:11:17 +0900

    Time: 543 ms (53.41%), Space: 31.1 MB (55.16%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,10 +1,9 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        # Memoization
-        sums = [nums[0]]
-        #print("Start sums as ", sums)
-        for i in range(1,len(nums)):
-            #print("i: ", i)
-            sums.append(nums[i]+ (sums[i-1] if sums[i-1]>0 else 0)) #sums[i-1]가 0보다 작으면 nums[i]로 새롭게 시작. 아니면 전꺼랑 합쳐서 누적 subarray구하기
-            #print("result sums : ",sums)
-        return max(sums)
+        best_sum = -10000
+        current_sum = 0
+        for num in nums:
+            current_sum = max(num, current_sum + num)
+            #print(best_sum, current_sum)
+            best_sum = max(best_sum, current_sum)
+        return best_sum

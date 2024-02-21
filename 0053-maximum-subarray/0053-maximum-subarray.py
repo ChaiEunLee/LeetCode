4b7af2633478d6f9301393c65eba4265ab8daca5
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-02-21 10:08:30 +0900

    Time: 516 ms (80.01%), Space: 31 MB (80.52%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,18 +1,10 @@
 class Solution:
-    def maxSubArray(self, nums):
-        def maxSubArray(A, L, R):
-            if L > R: return -inf #종료. 절대 largest가 될 수 없는 값을 반환
-            
-            mid, left_sum, right_sum, cur_sum = (L + R) // 2, 0, 0, 0 #init
-            
-            for i in range(mid-1, L-1, -1): # L: mid-1을 포함하는 subarray중에서 max subarray를 구함
-                left_sum = max(left_sum, cur_sum := cur_sum + A[i])
-            
-            cur_sum = 0
-            for i in range(mid+1, R+1): # R: mid+1를 포함하는 subarray중에서 max subarray를 구함
-                right_sum = max(right_sum, cur_sum := cur_sum + A[i])
-                
-            # L의 새로운 subarray, R의 새로운 subarray, mid를 포함하는 subarray 중 max
-            return max(maxSubArray(A, L, mid-1), maxSubArray(A, mid+1, R), left_sum + A[mid] + right_sum) 
-        
-        return maxSubArray(nums, 0, len(nums)-1)
+    def maxSubArray(self, nums: List[int]) -> int:
+        # Memoization
+        sums = [nums[0]]
+        #print("Start sums as ", sums)
+        for i in range(1,len(nums)):
+            #print("i: ", i)
+            sums.append(nums[i]+ (sums[i-1] if sums[i-1]>0 else 0)) #sums[i-1]가 0보다 작으면 nums[i]로 새롭게 시작. 아니면 전꺼랑 합쳐서 누적 subarray구하기
+            #print("result sums : ",sums)
+        return max(sums)

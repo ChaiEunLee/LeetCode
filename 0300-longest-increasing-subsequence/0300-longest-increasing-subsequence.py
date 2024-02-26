3075a647d2ee75aad8e4d7ebddcc5ab529bfc3dd
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-02-26 09:16:09 +0900

    Time: 1050 ms (67.37%), Space: 16.9 MB (95.98%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,12 +1,9 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        # dp list 만들고 각 index까지 increasing subsequence 길이 적기
-        # 지금 수보다 작은 숫자에다가 +1하기
-        dp = [1]*len(nums)
-        for i in range(1,len(nums)):
-            for prev in range(i-1,-1,-1):
-                #print(prev,i,nums[prev],nums[i])
-                if nums[prev]<nums[i] and dp[prev]>=dp[i]:
-                    dp[i] = dp[prev]+1
-                    #print(dp)       
+        n = len(nums)
+        dp = [1]*n
+        for i in range(n):
+            for j in range(i):
+                if nums[i]>nums[j] and dp[i]<dp[j]+1:
+                    dp[i] = dp[j] + 1
         return max(dp)

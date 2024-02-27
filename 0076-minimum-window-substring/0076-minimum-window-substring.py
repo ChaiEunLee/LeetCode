8a6511437d2b04b5d935fd5536b7c5c4ce88be5b
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-02-27 10:38:41 +0900

    Time: 2363 ms (5.00%), Space: 17.3 MB (51.20%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,28 +1,21 @@
 import collections
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        need = collections.Counter(t) #t element를 key, 개수를 value로 dict 만들어줌. 
-        missing = len(t)
-        left = start = end = 0
- 
+        t_count = collections.Counter(t)
+        current_count = collections.Counter()
+        
+        start = float('-inf')
+        end = float('inf')
+        
+        left = 0
         # move right pointer
-        for right, char in enumerate(s,1): # enumerate(s), index는 1부터
-            print(right, char)
-            missing -= need[char] > 0
-            need[char] -= 1
-            #print(missing, need)
+        for right, char in enumerate(s,1):
+            current_count[char] += 1
             
-            # move left pointer if missing is 0
-            # 왼쪽 포인터를 더 줄여서 subarray를 더 줄일 수 있는지 확인
-            if missing == 0:
-                while left < right and need[s[left]] < 0: # need가 음수면 left pointer가 엉뚱한 글자를 가리키고 있을테니 0까지 이동하기.
-                    need[s[left]] += 1
-                    left += 1
-                if not end or right-left <= end-start:
-                    start, end = left, right
-                    need[s[left]] += 1
-                    missing += 1
-                    left += 1
-                    #print(need, missing, left)
-                    
-        return s[start:end]
+            # AND 연산 결과로 왼쪽 포인터 이동 판단
+            while current_count & t_count == t_count:
+                if right-left < end-start:
+                    start,end = left,right
+                current_count[s[left]] -= 1
+                left += 1
+        return s[start:end] if end-start<=len(s) else ''

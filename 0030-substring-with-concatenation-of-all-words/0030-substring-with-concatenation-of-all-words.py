3a038946442d993d153c58bc888dddb7e036c523
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-02-29 09:44:23 +0900

    Time: 73 ms (86.38%), Space: 17.3 MB (86.59%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -1,27 +1,31 @@
-from collections import deque, defaultdict
+import collections
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
         word_len = len(words[0])
-        ori_word_dict = defaultdict(int) # 디폴트 값이 int인 딕셔너리
         
+        # Make original word dict
+        ori_word_dict = collections.defaultdict(int) 
         for word in words:
-            ori_word_dict[word] += 1 # 이미 dict에 있는지 없는지 체크 안해도 되고 += 1 해주면 됨..!
+            ori_word_dict[word] += 1
             
-        all_word_len = len(words) * word_len #s의 최소 길이
+        all_word_len = len(words)*word_len
         result = []
         
+        # 0,3,6,9 => 1,4,7,9 => 2,5,8,... (word 첫글자 돌고, 두번째글자 돌고 하는 방식)
         for i in range(word_len):
             queue = deque()
             word_dict = ori_word_dict.copy()
-            for j in range(i, len(s) - word_len + 1, word_len): # 시작지점 달리하면서 +word_len 만큼씩 띄어가며 읽기
-                word = s[j:j + word_len]
-                if word_dict.get(word, 0) != 0: #word가 없으면 0을 반환해주는데 != 0 : 그 단어가 있다면
-                    word_dict[word] -= 1 #-1해주고
-                    queue.append(word) #word가 있으면 queue에 넣어줌
-                    if sum(word_dict.values()) == 0: #word_dict에서 단어 다 빼면
-                        result.append(j - all_word_len + word_len)
-                        last_element = queue.popleft()
-                        word_dict[last_element] = word_dict.get(last_element, 0) + 1
+            for j in range(i,len(s)-word_len+1, word_len):
+                word = s[j:j+word_len]
+                # step
+                if word_dict.get(word,0) != 0: #get: word 찾고, 없으면 0을 넣는다
+                    word_dict[word] -= 1
+                    queue.append(word)
+                    # 다 된거같으면
+                    if sum(word_dict.values())==0:
+                        result.append(j-all_word_len+word_len) #index 넣기
+                        last_element = queue.popleft() #이제 옆으로 이동하려고 마지막에 넣은 단어 찾고
+                        word_dict[last_element] = word_dict.get(last_element,0) + 1 #그거 index+1부터 다시 시작
                 else:
                     while len(queue):
                         last_element = queue.popleft()
@@ -33,4 +37,5 @@ class Solution:
                             if word_dict[last_element] > ori_word_dict[last_element]:
                                 word_dict = ori_word_dict.copy()
         return result
-        
+                        
+            

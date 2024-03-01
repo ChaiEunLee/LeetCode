15321a2c7e0a47eb3a4408385593ca3d743eded0
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-03-01 09:08:40 +0900

    Time: 53 ms (84.37%), Space: 17.9 MB (56.39%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,8 +1,3 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        result = 0
-        # 값이 오르는 경우 매번 그리디 계산
-        for i in range(len(prices)-1):
-            if prices[i+1]>prices[i]:
-                result+=prices[i+1]-prices[i]
-        return result
+        return sum(max(prices[i+1]-prices[i],0) for i in range(len(prices)-1))

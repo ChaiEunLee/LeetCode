df507b0e5bb256ba9108c85702540688487daaba
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-03-02 11:21:13 +0900

    Time: 350 ms (90.46%), Space: 17 MB (97.07%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,20 +1,14 @@
 import collections
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        counter = collections.Counter(tasks)
-        result = 0
+        task_counts = list(collections.Counter(tasks).values()) # counts list
+        M = max(task_counts) # 제일 큰 counts 숫자
+        Mct = task_counts.count(M) # 제일 큰 counts인 tasks들
         
+        # Example 1
+        # (nums of chunks with idles * length of a chunk with idle) + length of the final chunk # 2*3+2=8
+        # nums of chunks with idles: task의 최대 interval = 3-1 = 2 = A A A면 interval은 2개니까
+        # length of a chunk with idle : n+1 = 2+1 = A __ A __ A 로 되어야하니까 A의 한 chunk는 3
+        # length of the final chunk : num of chars with max count = 2 = A,B 둘 다 최대길이니까 마지막에 나머지처럼 돌아갈것들
+        return max(len(tasks), (M - 1) * (n + 1) + Mct) # tasks길이, (제일 긴 task 길이 -1)*(interval+1) + 제일 큰 counts tasks
         
-        while True:
-            sub_count=0
-            # 개수 순 추출
-            for task, _ in counter.most_common(n+1): #최빈값 n+1개 반환. 
-                sub_count+=1
-                result+=1
-                counter.subtract(task) #counter[task]-=1
-                counter += collections.Counter() # 0이하인 아이템을 목록에서 완전히 제거. 0이하인것만 아예 빼주는 함수인듯.
-            if not counter:
-                break
-            result += n-sub_count+1
-        
-        return result

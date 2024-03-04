9a7a1cd3672c01fc931fae7361f7365fac714dec
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-03-04 10:24:32 +0900

    Time: 134 ms (57.64%), Space: 18.4 MB (93.91%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -1,13 +1,16 @@
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
-        g.sort()
-        s.sort()
-        
+        # Can only give one cookie to each child
+        # only if it is the same or bigger than each child's greed
+        # return children number who can get cookie
+        # O(n+m)
         result = 0
-        for i in s:
-            index = bisect.bisect_right(g,i) # g에서 cookie(=i)의 right index를 찾음. 작아서 없으면 0 출력.
-            # children의 index인거니까 result보다 크면 쿠키를 줄 수 있다는 의미라서 result += 1. 어쩌면 result를 index 처럼 취급하는 걸지도.
-            if index>result:
-                result += 1
-            #print(index, g, i, result)
-        return result
+        g = sorted(g)
+        s = sorted(s)
+        #print(g)
+        i=j=0
+        while i < len(g) and j < len(s):
+            if g[i] <= s[j]:
+                i += 1
+            j += 1
+        return i

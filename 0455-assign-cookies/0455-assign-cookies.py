0129914f0e445f168a13efad27d944e0339b78ee
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-03-04 10:17:04 +0900

    Time: 125 ms (92.84%), Space: 18.4 MB (93.91%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -1,22 +1,12 @@
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
-        # Can only give one cookie to each child
-        # only if it is the same or bigger than each child's greed
-        # return children number who can get cookie
-        # O(n+m)
-        result = 0
-        g = sorted(g)
-        s = sorted(s)
-        #print(g)
-        for i in range(len(g)):
-            j = 0
-            while j<len(s):
-                if g[i]<=s[j]:
-                    result += 1
-                    s.remove(s[j])
-                    #print(s)
-                    break
-                else:
-                    j+=1
-            #print(result, s)
-        return result
+        g.sort()
+        s.sort()
+        
+        child_i = cookie_j = 0
+        
+        while child_i < len(g) and cookie_j < len(s):
+            if s[cookie_j] >= g[child_i]:
+                child_i += 1
+            cookie_j += 1
+        return child_i

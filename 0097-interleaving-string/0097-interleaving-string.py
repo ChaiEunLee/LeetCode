dfa1383c85f9de3dc58b76df76afc8e574ee37ab
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-03-06 08:50:35 +0900

    Time: 39 ms (76.47%), Space: 17.6 MB (23.30%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -3,16 +3,15 @@ class Solution:
         r, c, l = len(s1), len(s2), len(s3)
         if r+c != l:
             return False
-        stack, visited = [(0,0)], set((0,0)) #s1,s2 index
-        while stack: 
-            # stack을 다 쓸때까지 == x,y index가 끝까지 돌 때까지
-            x,y = stack.pop()
+        queue, visited = [(0,0)], set((0,0)) #s1,s2 index
+        while queue: 
+            x,y = queue.pop(0)
             if x+y==l:
                 return True
             if x+1<=r and s1[x] == s3[x+y] and (x+1,y) not in visited:
-                stack.append((x+1,y))
+                queue.append((x+1,y))
                 visited.add((x+1,y))
             if y+1<=c and s2[y] == s3[x+y] and (x,y+1) not in visited:
-                stack.append((x,y+1))
+                queue.append((x,y+1))
                 visited.add((x,y+1))
         return False

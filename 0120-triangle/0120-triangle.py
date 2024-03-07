327685402efa499543b8598be629b6e66a4b11c1
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-03-07 14:04:13 +0900

    Time: 61 ms (63.44%), Space: 17.6 MB (85.39%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,15 +1,17 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        memo = [[-1]*len(triangle) for _ in range(len(triangle))]
-        def dfs(i,j):
-            if i==len(triangle):
-                return 0
-            if memo[i][j] != -1:
-                return memo[i][j]
-            
-            lower_left = triangle[i][j] + dfs(i+1,j)
-            lower_right = triangle[i][j] + dfs(i+1, j+1)
-            memo[i][j] = min(lower_left, lower_right)
-            return memo[i][j]
-        
-        return dfs(0,0)
+        # 맨 마지막 줄에 값들 다 넣고 그 중에서 하나 꼽는거
+        n = len(triangle)
+        next_row = triangle[-1][:]
+        curr_row = [0]*n
+        for i in range(n-2,-1,-1):
+            for j in range(i+1): #0~i
+                #print(i,j)
+                lower_left = triangle[i][j] + next_row[j] #왼
+                lower_right = triangle[i][j] + next_row[j+1] #오
+                curr_row[j] = min(lower_left, lower_right) # 둘중에 작은값으로
+                # 맨 마지막 줄 기준으로 하니까 위로 올라가면 index가 넘어가지 않음
+                # + 작은값을 위로 올리니까 맨 앞 값이 제일 작은 값
+                #print(curr_row, next_row)
+            curr_row, next_row = next_row, curr_row
+        return next_row[0] 

fccb62baa953f02a72f8e42a5b8afb1befb82fbb
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-03-07 09:11:48 +0900

    Time: 64 ms (45.03%), Space: 18 MB (41.37%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,14 +1,15 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        for i in range(1,len(triangle)):
-            for j in range(len(triangle[i])):
-                #print(i,j)
-                if j==0:
-                    # 가장자리
-                    triangle[i][j] = triangle[i][j] + triangle[i-1][j]
-                elif j==len(triangle[i])-1:
-                    triangle[i][j] = triangle[i][j] + triangle[i-1][j-1]
-                else:
-                    triangle[i][j] = triangle[i][j] + min(triangle[i-1][j-1], triangle[i-1][j])
-            #print(triangle)
-        return min(triangle[-1])
+        memo = [[-1]*len(triangle) for _ in range(len(triangle))]
+        def dfs(i,j):
+            if i==len(triangle):
+                return 0
+            if memo[i][j] != -1:
+                return memo[i][j]
+            
+            lower_left = triangle[i][j] + dfs(i+1,j)
+            lower_right = triangle[i][j] + dfs(i+1, j+1)
+            memo[i][j] = min(lower_left, lower_right)
+            return memo[i][j]
+        
+        return dfs(0,0)

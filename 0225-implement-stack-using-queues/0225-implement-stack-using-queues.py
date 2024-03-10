596f977e32fa772e2384f7279ecc1ee5a93c27e0
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-03-10 10:32:20 +0900

    Time: 37 ms (43.97%), Space: 16.6 MB (37.38%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -1,29 +1,22 @@
-import collections
+from collections import deque
 class MyStack:
 
     def __init__(self):
-        self.q = collections.deque()
-        
-    def push(self, x: int):
-        self.q.append(x) 
-        for _ in range(len(self.q) - 1):
-            self.q.append(self.q.popleft()) #맨 뒤 빼고는 다 popleft로 빼서 맨 뒤로 append하기
+        self.stack = deque()
 
+    def push(self, x: int) -> None:
+        self.stack.append(x)
 
     def pop(self) -> int:
-        return self.q.popleft()
-        
+        return self.stack.pop() #deque, pop last element()
+
     def top(self) -> int:
-        top = self.q[0]
+        top = self.stack[-1]
         return top
 
     def empty(self) -> bool:
-        if self.q:
-            return False
-        else:
-            return True
-        
-
+        return len(self.stack)==0
+            
 
 # Your MyStack object will be instantiated and called as such:
 # obj = MyStack()

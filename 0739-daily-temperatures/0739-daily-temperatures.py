6992a5095d55df5823e5fed03956e19c49dccee7
ChaiEunLee <85481704+ChaiEunLee@users.noreply.github.com>
2024-03-11 09:24:03 +0900

    Time: 850 ms (97.62%), Space: 31.4 MB (45.39%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,17 +1,21 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # 더 따뜻한 날까지 기다리는 기간. 끝은 무조건 0.
-        # stack으로 쌓고 빼면서 기온 올라갔으면 빼고 아니면 놔두기.
-        tempstack = []
+        # number of days you have to wait after the ith day to get warmer temperature
+        # stack
+        # 현재보다 기온이 올라가면 pop 아니면 놔두기?????그게 무슨말이지?
+        
+        # temperatures를 돌면서 
+        # 오늘 기온보다 높아진 거 있으면 pop 후 오늘 기온 append
+        
+        tempstack = [0]
         days = [0]*len(temperatures)
-        # 날짜를 돌면서
+        
         for i in range(len(temperatures)):
-            # 오늘 기온이 더 높으면 계속 pop(). 
-            while tempstack and temperatures[tempstack[-1]] < temperatures[i]:
+            #print(i, temperatures[tempstack])
+            while tempstack and temperatures[tempstack[-1]] < temperatures[i]: #과거기온<현재기온
+                # 현재기온보다 낮은 기온들을 넣어주는 방식
                 last = tempstack.pop()
                 days[last] = i-last
-            # 오늘 기온을 stack에 append
             tempstack.append(i)
-            #print(i, temperatures[i], tempstack, days)
-        
+            
         return days
